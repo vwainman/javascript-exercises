58a054e0a159f9043203e4e77cf0c716bf7a42b6
Vincent Wainman <vincewainman@gmail.com>
2022-04-18 13:29:02 -0600

    05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+    if (num1 < 0 || num2 < 0
+        || typeof num1 != "number"
+        || typeof num2 != "number") {
+        return "ERROR"
+    }
+    let sum = 0;
+    const largest = Math.max(num1, num2);
+    const smallest = Math.min(num1, num2);
+    for (let i = smallest; i <= largest; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
