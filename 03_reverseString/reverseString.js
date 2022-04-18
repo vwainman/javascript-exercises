549d8e5284c4d91815593edd6f1f387314d876f1
Vincent Wainman <vincewainman@gmail.com>
2022-04-18 13:05:22 -0600

    complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    if (string === "") {
+        return ""
+    }
+    let reversedString = ""
+    for (let i = string.length - 1; i >= 0; i--) {
+        reversedString += string[i]
+    }
+    return reversedString
 };
 
 // Do not edit below this line
