5974be3b57d3882ae34fb51d1de8138d67c03642
Vincent Wainman <vincewainman@gmail.com>
2022-04-18 12:59:11 -0600

    complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, number) {
+    let repeatedString = ""
+    if (number < 0) {
+        return "ERROR"
+    }
+    for (let i = 0; i < number; i++) {
+        repeatedString += string
+    }
+    return repeatedString
 };
 
 // Do not edit below this line
