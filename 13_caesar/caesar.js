4f0312833c83a7441766ecade990bcca8767e934
Vincent Wainman <vincewainman@gmail.com>
2022-04-20 14:32:06 -0600

    complete 13_caesar

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,22 @@
-const caesar = function() {
-
+const caesar = function (string, shift_num) {
+    // Wrapping around
+    if (shift_num < 0) {
+        return caesar(string, shift_num + 26);
+    }
+    let transformedString = "";
+    for (let i = 0; i < string.length; i++) {
+        let char = string[i]
+        let unicode = char.charCodeAt();
+        if (char.match(/[a-z]/)) {
+            char = String.fromCharCode(((unicode - 97 + shift_num) % 26) + 97);
+        }
+        else if (char.match(/[A-Z]/)) {
+            char = String.fromCharCode(((unicode - 65 + shift_num) % 26) + 65);
+        }
+        transformedString += char;
+    }
+    return transformedString;
 };
-
+caesar('Hello, World!', 5);
 // Do not edit below this line
 module.exports = caesar;
