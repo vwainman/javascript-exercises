ab2ef386f5eb65a0c9956db89efe9d462f900b3f
Vincent Wainman <vincewainman@gmail.com>
2022-04-20 13:50:46 -0600

    complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    let oldestPerson = null;
+    let oldestAge = 0;
+    for (person of people) {
+        let personAge = (person.yearOfDeath || 2022) - person.yearOfBirth;
+        if (personAge > oldestAge) {
+            oldestPerson = person
+            oldestAge = personAge
+        }
+    }
+    return oldestPerson
 };
 
 // Do not edit below this line
